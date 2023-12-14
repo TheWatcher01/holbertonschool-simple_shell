d440b5636516ca6eb8fd6fcbed72b33c1a3162df
TheWatcher01 <teddydeberdt@gmail.com>
2023-12-14 09:54:08 -0800

    re_add_shell_checked_version.c

--- shell.c
+++ shell.c
@@ -1,20 +1,26 @@
 #include "main.h"
+
 /**
- * main - Entry point (simple shell example)
- * Return: 0 on success, 1 on error.
+ * main - main function
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: Shell.
  */
-int main(void)
+int main(int argc, char *argv[])
 {
 	pid_t child_pid;
 	int status, read;
-	char *command = NULL, *argv[2];
+	char *command = NULL, *argv_exec[2];
 	size_t len = 0;
+	(void)argc;
 
 	while (1)
 	{
-		printf("#cisfun$ ");
+		if (isatty(STDIN_FILENO))
+			printf("#cisfun$ ");
 		read = getline(&command, &len, stdin);
-		if (read == -1)
+		if (read == -1 || feof(stdin))
 		{
 			free(command);
 			return (0);
@@ -29,11 +35,11 @@ int main(void)
 		child_pid = fork();
 		if (child_pid == 0)
 		{
-			argv[0] = command;
-			argv[1] = NULL;
-			if (execve(argv[0], argv, environ) == -1)
+			argv_exec[0] = command;
+			argv_exec[1] = NULL;
+			if (execve(argv_exec[0], argv_exec, environ) == -1)
 			{
-				perror(command);
+				perror(argv[0]);
 				free(command);
 				exit(0);
 			}
