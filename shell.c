a3290337c49d101845aae2b5cbb8adf6c999f61e
TheWatcher01 <teddydeberdt@gmail.com>
2023-12-14 09:28:57 -0800

    test_new_shell.c_file_v2

--- shell.c
+++ shell.c
@@ -7,38 +7,33 @@ int main(void)
 {
 	pid_t child_pid;
 	int status;
-	char command[1024];
+	char *command = NULL;
+	size_t len = 0;
 	char *argv[2];
+	int read;
 
-	while (1)
+	printf("#cisfun$ ");
+	while ((read = getline(&command, &len, stdin)) != -1)
 	{
-		printf("#cisfun$ ");
-		if (fgets(command, 1024, stdin) == NULL)
-		{
-			printf("\n");
-			exit(0);
-		}
 		command[strcspn(command, "\n")] = 0;
 		child_pid = fork();
-		if (child_pid == -1)
-		{
-			perror("Error:");
-			return (1);
-		}
 		if (child_pid == 0)
 		{
 			argv[0] = command;
 			argv[1] = NULL;
 			if (execve(argv[0], argv, environ) == -1)
-			{
-				perror(command);
-				exit(0);
-			}
+				perror(command), free(command), exit(0);
 		}
-		else
+		else if (child_pid > 0)
 		{
 			wait(&status);
+			if (read != -1)
+				printf("#cisfun$ ");
 		}
+		free(command);
+		command = NULL;
 	}
+	printf("\n");
+	free(command);
 	return (0);
 }
