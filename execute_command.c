21f17ce59a4d1f643218b33bc455e08229a2f3e5
TheWatcher01 <teddydeberdt@gmail.com>
2023-12-15 09:40:02 -0800

    restor_best_version

--- execute_command.c
+++ execute_command.c
@@ -1,42 +1,43 @@
 #include "main.h"
 /**
- * execute_command - Executes a command
- * @argv_exec: The command to execute
+ * execute_command - Execute a command with arguments
+ * @argv_exec: Null-terminated array of arguments
  *
- * Return: Nothing
+ * This function takes a null-terminated array of arguments as input and
+ * executes the command specified by the first argument. The remaining
+ * arguments are passed to the command. The function uses fork to create a
+ * new process and execve to replace the new process with the command to be
+ * executed. If the command cannot be executed, an error message is printed
+ * and the program exits.
  */
 void execute_command(char **argv_exec)
 {
 	pid_t child_pid;
-	int status;
-	char *command_path;
 
-	command_path = get_command_path(argv_exec[0]);
-	if (command_path == NULL)
-	{
-		fprintf(stderr, "%s: command not found\n", argv_exec[0]);
-		return;
-	}
-
+	/* Create a new process */
 	child_pid = fork();
-	if (child_pid == 0)
+
+	if (child_pid == -1)
 	{
-		if (execve(command_path, argv_exec, environ) == -1)
-		{
-			perror("Error:");
-		}
+		/* If fork failed, print an error message and exit */
+		perror("Error:");
+		free(argv_exec[0]);
 		exit(EXIT_FAILURE);
 	}
-	else if (child_pid < 0)
+	else if (child_pid == 0)
 	{
-		perror("Error:");
+		/* In the child process, replace the process with the command */
+		if (execve(argv_exec[0], argv_exec, environ) == -1)
+		{
+			/* If execve failed, print an error message and exit */
+			fprintf(stderr, "./hsh: %s: No such file or directory\n", argv_exec[0]);
+			free(argv_exec[0]);
+			exit(0);
+		}
 	}
-	else
+	else if (child_pid > 0)
 	{
-		do {
-			waitpid(child_pid, &status, WUNTRACED);
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+		/* In the parent process, wait for the child to finish */
+		wait(NULL);
 	}
-
-	free(command_path);
 }
--- main.h
+++ main.h
@@ -17,7 +17,9 @@ void prompt(void);
 char *read_command(char **command, size_t *len);
 void execute_command(char **argv_exec);
 char **parse_command(char *command);
-char *get_command_path(char *command);
+
+
+/* En attente de test : char *get_command_path(char *command); */
 
 /** Structure Prototypes */
 
